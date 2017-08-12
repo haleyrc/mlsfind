93bf8f90469a3422c44a1011b31206b4b5d781b5
Ryan Haley <ryanhaley@frazer.biz>
2017-08-12 11:41:45 -0400

    Output more relevant stats

--- main.go
+++ main.go
@@ -5,17 +5,28 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
 func main() {
-	if len(os.Args) != 2 {
-		fmt.Println("Usage: mlsfind MLSID")
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: mlsfind MLSID...")
 		os.Exit(2)
 	}
 
-	url := fmt.Sprintf("http://www.slcmls.com/Search/ListingDetail.aspx?org_id=nystlc&mls_property_id=%s", os.Args[1])
+	for i := 1; i < len(os.Args); i++ {
+		print(os.Args[i])
+		fmt.Println()
+	}
+}
+
+func print(mls string) {
+	url := fmt.Sprintf("http://www.slcmls.com/Search/ListingDetail.aspx?org_id=nystlc&mls_property_id=%s", mls)
+	fmt.Println("MLS#:\t\t\t", mls)
+	fmt.Println("Link:\t\t\t", url)
+
 	client := &http.Client{
 		CheckRedirect: func(req *http.Request, via []*http.Request) error {
 			return http.ErrUseLastResponse
@@ -50,10 +61,19 @@ func main() {
 	// 	})
 	// })
 
+	fmt.Println("Price:\t\t\t", doc.Find(".price").First().Text())
+	fmt.Println("Square Feet:\t\t", strings.TrimSuffix(doc.Find(".sqft").First().Text(), " sqft"))
+	fmt.Println("Bedrooms/Bathrooms:\t", doc.Find(".bed-baths").First().Children().Remove().End().Text())
+
 	doc.Find(".details-text-data").Each(func(i int, s *goquery.Selection) {
 		label := s.Children().First()
-		if label.Text() == "Tax Amount:" {
-			fmt.Println(label.Text(), label.SiblingsFiltered("span").First().Text())
+		switch label.Text() {
+		case "Tax Amount:", "Lot Size:":
+			fmt.Println(label.Text()+"\t\t", label.SiblingsFiltered("span").First().Text())
+		case "Tax Assessment:":
+			fmt.Printf(label.Text()+"\t\t $%s\n", strings.TrimPrefix(label.Parent().Text(), label.Text()))
+		case "Year Built:":
+			fmt.Println(label.Text()+"\t\t", strings.TrimPrefix(label.Parent().Text(), label.Text()))
 		}
 	})
 }
